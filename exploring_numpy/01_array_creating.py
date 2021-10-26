472ad112ec552d4be5820bd75ab899bb0974b244
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-10-26 13:03:55 +0300

    array attributes

--- exploring_numpy/01_array_creating.py
+++ exploring_numpy/01_array_creating.py
@@ -34,3 +34,32 @@ np.random.random((3, 3))
 np.random.normal(0, 1, (3, 3)) # 0 - median, 1 - std
 np.eye(3)
 
+##############################
+#     ARRAY'S ATTRIBUTES
+##############################
+
+# начальное значение для воспроизводимости
+np.random.seed(0)
+
+x1 = np.random.randint(10, size=6) # n=1
+x2 = np.random.randint(10, size=(4, 5)) # n=2
+x3 = np.random.randint(10, size=(3, 4, 5)) # n=3
+
+# main attributes
+for example in [x1, x2, x3]:
+    meaning_str = "dim = {}, shape = {}, size = {}, dtype = {}".format(
+        example.ndim, example.shape, example.size, example.dtype)
+    print(meaning_str)
+
+# more attributes
+for example in [x1, x2, x3]:
+    meaning_str = "itemsize = {}, nbytes = {}".format(
+        example.itemsize, example.nbytes)
+    print(meaning_str)
+
+
+
+
+
+
+
